3d68eae2d6021333716e05c0df19402c66aa1e6d
Nathan Files <nathanwfiles@gmail.com>
2018-01-11 14:42:49 -0500

    fixes an async bug in the rate-limiter
    - weirdness was happening between Promise (async) and Observable (immediate) with managing the "batch size" of requests

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,44 @@
+import groupBy from 'lodash/groupBy';
+import sortBy from 'lodash/sortBy';
+
 import { RateLimiter } from './rate-limiter';
 
 const maxTimePeriodRequests = 5;
 const timePeriod = 1000;
 const maxConcurrentActions = 10;
-const rateLimiter = new RateLimiter(maxTimePeriodRequests, timePeriod, maxConcurrentActions);
+
+const rateLimiter = new RateLimiter(timePeriod, maxTimePeriodRequests, maxConcurrentActions);
+
 const actionTime = 100;
 const actionCount = 30;
 
 const main = async () => {
-    console.log('starting:');
+    const start = Date.now();
 
     // const action = (i: number) => new Promise<number>(resolve => {
     //     setTimeout(() => {
-    //         console.log(`finished ${i}`);
-    //         resolve(i);
+    //         const secondsElapsed = (Date.now() - start) / 1000;
+    //         resolve(secondsElapsed);
     //     }, 500) // Math.random() * 250 + 500)
     // });
 
     const action = async (i: number) => {
-        console.log(`finished ${i}`);
+        const secondsElapsed = (Date.now() - start) / 1000;
+        return secondsElapsed;
     }
 
     const actions = new Array(actionCount)
         .fill(0)
-        .map((_, i) =>
-            rateLimiter.run(() => action(i))
-        );
+        .map((_, i) => {
+            return rateLimiter.run(() => action(i));
+        });
 
-    // console.log(
-    await Promise.all(actions)
-    // );
-    console.log('done!');
+    const results = await Promise.all(actions)
+    const grouped = groupBy(results, r => Math.floor(r));
+    const ordered = sortBy(Object.keys(grouped), Number)
+        .forEach(seconds => {
+            console.log(`${seconds} seconds: ${grouped[seconds].length}`);
+        });
 };
 
 main().catch(ex => {
--- src/rate-limiter.ts
+++ src/rate-limiter.ts
@@ -1,17 +1,20 @@
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/combineLatest';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/skip';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/takeUntil';
 import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/observable/combineLatest';
 
-
+/**
+ * Service for managing rate-limited actions, especially API requests
+ *
+ * @export
+ * @class RateLimiter
+ */
 export class RateLimiter {
-    public totalRequests = 0;
-
     private _waiting = 0;
 
     private readonly _timePeriodActions$ = new Subject<number>();
@@ -19,11 +22,11 @@ export class RateLimiter {
     private _timePeriodActions = 0;
     private _concurrentActions = 0;
 
-    private _isDestroyed = new Subject();
+    private _isDestroyed = new Subject<void>();
 
     constructor(
-        public readonly maxTimePeriodActions: number,
         public readonly timePeriod: number,
+        public readonly maxTimePeriodActions: number,
         public readonly maxConcurrentActions: number,
     ) {
         this._timePeriodActions$
@@ -34,30 +37,41 @@ export class RateLimiter {
             .subscribe(value => this._concurrentActions = value);
     }
 
+    /**
+     * Run an action within the rate limit. It will be deferred if it cannot be evaluated immediately.
+     *
+     * @template T
+     * @param {(() => (T | PromiseLike<T>))} action
+     * @returns {Promise<T>}
+     * @memberof RateLimiter
+     */
     async run<T>(action: () => (T | PromiseLike<T>)): Promise<T> {
-        // if it is valid to run the task right now, do so
+        // run it immediately, if possible
         if (this._timePeriodActions < this.maxTimePeriodActions &&
             this._concurrentActions < this.maxConcurrentActions) {
             return this._run(action);
         }
 
+        // defer the action
         const currentlyWaiting = this._waiting;
         this._waiting = this._waiting + 1;
 
-        await Observable
+        return await Observable
             .combineLatest(this._timePeriodActions$, this._concurrentActions$)
-            // .delay(100)
             .filter(([timePeriod, concurrent]) =>
                 timePeriod < this.maxTimePeriodActions &&
                 concurrent < this.maxConcurrentActions
             )
             .skip(currentlyWaiting)
             .take(1)
+            .map(() => {
+                this._waiting = this._waiting - 1;
+                return this._run(action);
+            })
+            // convert the observable to a promise as *late* as possible.
+            // Promises are async, and we want to update the _waiting value as soon as possible
+            // weird things can happen when Promises and Observables mix
             .toPromise();
-
-        this._waiting = this._waiting - 1;
-
-        return this._run(action);
     }
 
     destroy(): void {
@@ -68,30 +82,16 @@ export class RateLimiter {
     private async _run<T>(action: () => (T | PromiseLike<T>)): Promise<T> {
         // track time period
         this._timePeriodActions$.next(this._timePeriodActions + 1);
-        this._delayTimePeriod().then(() => {
-            this._timePeriodActions$.next(this._timePeriodActions - 1);
-        });
-
-        this.totalRequests++;
+        setTimeout(
+            () => this._timePeriodActions$.next(this._timePeriodActions - 1),
+            this.timePeriod,
+        );
 
         // track concurrent actions
         this._concurrentActions$.next(this._concurrentActions + 1);
-
-        // try {
         const result = await action();
-        // } catch (ex) {
-        // await new Promise(resolve => setTimeout(resolve, 1000));
-        // return await this.run<T>(action);
-        // } finally {
         this._concurrentActions$.next(this._concurrentActions - 1);
+
         return result;
-        // }
-
-    }
-
-    private _delayTimePeriod() {
-        return new Promise<void>(
-            resolve => setTimeout(resolve, this.timePeriod)
-        );
     }
 }
